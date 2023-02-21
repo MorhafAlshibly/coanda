4308c43e8364099acbb406f84f47bc96512cf8f9
Morhaf Alshibly <morhaf.alshibly@outlook.com>
2023-02-21 04:02:01 +0000

    Removed unnecessary packages

--- src/utils/run-dev.ts
+++ src/utils/run-dev.ts
@@ -9,7 +9,7 @@ for (const path of paths) {
 	const directories = path.split("\\");
 	const name = directories[directories.length - 2];
 	commands.push({
-		command: "ts-node-dev --respawn --transpile-only -r dotenv/config " + path,
+		command: "ts-node-dev -r dotenv/config " + path,
 		name: name.charAt(0).toUpperCase() + name.slice(1),
 	});
 }
