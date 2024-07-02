930ff6fd498be450c7aac423ae80b7fb7124efd6
Morhaf Alshibly <morhaf.alshibly@outlook.com>
2024-07-02 20:43:15 +0100

    added log input and output data

--- pkg/invokers/log.go
+++ pkg/invokers/log.go
@@ -2,9 +2,9 @@ package invokers
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
-	"runtime/trace"
 
 	"github.com/google/uuid"
 	"github.com/rs/zerolog"
@@ -36,32 +36,22 @@ func (i *LogInvoker) SetInvoker(invoker Invoker) *LogInvoker {
 func (i *LogInvoker) Invoke(ctx context.Context, command Command) error {
 	requestID := uuid.New().String()
 	ctx = context.WithValue(ctx, requestIDKey, requestID)
-	log.Info().Str("requestId", requestID).Msg(fmt.Sprintf("Command %T started", command))
-
-	// Start a trace region
-	traceFile, err := os.Create(fmt.Sprintf("trace_%s.out", requestID))
+	input, err := json.Marshal(command)
 	if err != nil {
-		log.Error().Err(err).Str("requestId", requestID).Msg("Failed to create trace file")
+		log.Error().Err(err).Str("requestId", requestID).Msg(fmt.Sprintf("Command %T errors", command))
 		return err
 	}
-	defer traceFile.Close()
-
-	if err := trace.Start(traceFile); err != nil {
-		log.Error().Err(err).Str("requestId", requestID).Msg("Failed to start trace")
-		return err
-	}
-	defer trace.Stop()
-
-	// Create a region to trace the invocation
-	region := trace.StartRegion(ctx, fmt.Sprintf("Invoke %T", command))
-	defer region.End()
-
+	log.Info().Str("requestId", requestID).Str("input", string(input)).Msg(fmt.Sprintf("Command %T started", command))
 	err = i.invoker.Invoke(ctx, command)
 	if err != nil {
 		log.Error().Err(err).Str("requestId", requestID).Msg(fmt.Sprintf("Command %T errors", command))
 		return err
 	}
-
-	log.Info().Str("requestId", requestID).Msg(fmt.Sprintf("Command %T executed and output returned", command))
+	output, err := json.Marshal(command)
+	if err != nil {
+		log.Info().Str("requestId", requestID).Msg(fmt.Sprintf("Command %T executed and output returned", command))
+		return nil
+	}
+	log.Info().Str("requestId", requestID).Str("output", string(output)).Msg(fmt.Sprintf("Command %T executed and output returned", command))
 	return nil
 }
